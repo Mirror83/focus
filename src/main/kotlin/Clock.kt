1fcc64848097d55b3de7ef3f4d65621af52dbae6
Mirror83 <glenochieng045@gmail.com>
2023-11-26 23:10:27 +0300

    Added support for notifications

--- src/main/kotlin/Clock.kt
+++ src/main/kotlin/Clock.kt
@@ -2,6 +2,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -9,19 +10,30 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.TrayState
+import androidx.compose.ui.window.rememberNotification
+import kotlinx.coroutines.launch
+
 
 @Composable
 @Preview
-fun Clock() {
+fun Clock(trayState: TrayState) {
+    val pomodoroDoneNotification =
+        rememberNotification("Focus", "Pomodoro ended")
+
     val timer = remember {
         Timer()
     }
+
 
     if (!timer.isPaused() && timer.isStarted()) {
         LaunchedEffect(timer) {
             timer.tick()
             timer.stop()
+            println("Send notification")
+            launch {
+                trayState.sendNotification(pomodoroDoneNotification)
+            }
         }
     }
 
@@ -35,7 +47,7 @@ fun Clock() {
             modifier = Modifier
                 .fillMaxWidth()
         ) {
-            Text(timer.output(), fontSize = 30.sp)
+            Text(timer.output(), style = MaterialTheme.typography.h2)
         }
 
         Row(
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,51 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.window.*
 
 @Composable
 @Preview
-fun App() {
+fun App(trayState: TrayState) {
     MaterialTheme {
-        Clock()
+        Clock(trayState)
     }
 }
 
 fun main() = application {
-    Window(title="focus", onCloseRequest = ::exitApplication) {
-        App()
+    val trayState = rememberTrayState()
+
+    Tray(icon = TrayIcon, state = trayState)
+
+    Window(
+        title = "focus",
+        onCloseRequest = ::exitApplication,
+        icon = FocusAppIcon
+    ) {
+        App(trayState)
+    }
+}
+
+/** Dummy app icon **/
+object FocusAppIcon : Painter() {
+    override val intrinsicSize = Size(256f, 256f)
+
+    override fun DrawScope.onDraw() {
+        drawOval(Color.Green, Offset(size.width / 4, 0f), Size(size.width / 2f, size.height))
+        drawOval(Color.Blue, Offset(0f, size.height / 4), Size(size.width, size.height / 2f))
+        drawOval(Color.Red, Offset(size.width / 4, size.height / 4), Size(size.width / 2f, size.height / 2f))
+    }
+}
+
+/** Dummy tray icon **/
+object TrayIcon : Painter() {
+    override val intrinsicSize = Size(256f, 256f)
+
+    override fun DrawScope.onDraw() {
+        drawOval(Color(0xFFFFA500))
     }
 }
