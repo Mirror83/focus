56ead276f43d7e668ccc51307ca097e5c6587e33
Mirror83 <glenochieng045@gmail.com>
2023-12-02 00:47:26 +0300

    Called createNotificationChannel()

--- composeApp/src/androidMain/kotlin/com/app/focus/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/app/focus/MainActivity.kt
@@ -1,24 +1,29 @@
 package com.app.focus
 
-import App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.app.focus.notifications.createTimerNotificationChannel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // One must create the notification channel before posting
+        // any notifications.
+        createTimerNotificationChannel(this)
+
         setContent {
             ClockAndroid()
         }
     }
+
 }
 
 @Preview
 @Composable
 fun AppAndroidPreview() {
-    App()
+    ClockAndroid()
 }
