8ecea403383368fa41e934d7cbb4a6684cad806e
Mirror83 <glenochieng045@gmail.com>
2023-12-02 00:47:54 +0300

    Added button that spawns a notification when clicked

--- composeApp/src/androidMain/kotlin/com/app/focus/ClockAndroid.kt
+++ composeApp/src/androidMain/kotlin/com/app/focus/ClockAndroid.kt
@@ -2,12 +2,19 @@ package com.app.focus
 
 import ClockComponents
 import Timer
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import com.app.focus.notifications.makeTimerNotification
 
 @Composable
 fun ClockAndroid() {
+    val context = LocalContext.current
+    val appName = stringResource(id = R.string.app_name)
     val timer = remember {
         Timer(1)
     }
@@ -19,4 +26,14 @@ fun ClockAndroid() {
         }
     }
     ClockComponents(timer = timer)
+    Button(onClick = {
+        makeTimerNotification(
+            appName,
+            "Button pushed",
+            context
+        )
+    }) {
+        Text("Press for notification")
+    }
 }
+
