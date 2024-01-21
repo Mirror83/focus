d94441a675d2caff07a4d49205e0674efea335ac
Mirror83 <glenochieng045@gmail.com>
2024-01-21 14:50:41 +0300

    Updated to use PomodoroTimer

--- composeApp/src/androidMain/kotlin/com/app/focus/ClockAndroid.kt
+++ composeApp/src/androidMain/kotlin/com/app/focus/ClockAndroid.kt
@@ -1,39 +1,50 @@
 package com.app.focus
 
 import ClockComponents
-import Timer
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import PomodoroTimer
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import com.app.focus.notifications.makeTimerNotification
 
 @Composable
 fun ClockAndroid() {
     val context = LocalContext.current
-    val appName = stringResource(id = R.string.app_name)
+
     val timer = remember {
-        Timer(1)
+        PomodoroTimer(2,1,3)
     }
 
     if (!timer.isPaused() && timer.isStarted()) {
         LaunchedEffect(timer) {
             timer.tick()
-            timer.stop()
+
+            timer.apply {
+                if (isStarted() && isOnBreak()) {
+                    if (isOnLongBreak())
+                        makeTimerNotification(
+                        "Four work sessions complete",
+                        "Time for a long break. You deserve it!",
+                        context
+                    ) else
+                        makeTimerNotification(
+                        "Work session complete",
+                        "Time for a quick break",
+                        context
+                    )
+                } else if (isStarted() && !isOnBreak()) {
+                    makeTimerNotification(
+                        "Break complete",
+                        "Time to go back to work",
+                        context
+                    )
+                }
+            }
         }
     }
+
     ClockComponents(timer = timer)
-    Button(onClick = {
-        makeTimerNotification(
-            appName,
-            "Button pushed",
-            context
-        )
-    }) {
-        Text("Press for notification")
-    }
+
 }
 
