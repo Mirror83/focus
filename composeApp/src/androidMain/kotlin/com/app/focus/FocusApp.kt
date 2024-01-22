8fb3a47c53e39c422c2817201906eeee5331448a
Mirror83 <glenochieng045@gmail.com>
2024-01-22 21:42:14 +0300

    Added navigation logic

---
+++ composeApp/src/androidMain/kotlin/com/app/focus/FocusApp.kt
@@ -0,0 +1,44 @@
+package com.app.focus
+
+import FocusScreen
+import HomeScreen
+import SettingsScreen
+import StatsScreen
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun FocusApp() {
+    Scaffold { innerPadding: PaddingValues ->
+        val navController = rememberNavController()
+        NavHost(
+            navController = navController,
+            startDestination = FocusScreen.Home.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(FocusScreen.Home.name) {
+                HomeScreen(
+                    onSettingsClicked = { navController.navigate(FocusScreen.Settings.name) },
+                    onStatsClicked = { navController.navigate(FocusScreen.Statistics.name) },
+                    onTimerClicked = { navController.navigate(FocusScreen.Timer.name) }
+                )
+            }
+            composable(FocusScreen.Timer.name) {
+                ClockAndroid()
+            }
+            composable(FocusScreen.Statistics.name) {
+                StatsScreen(onNavigateBack = { navController.navigateUp() })
+            }
+            composable(FocusScreen.Settings.name) {
+                SettingsScreen(onNavigateBack = { navController.navigateUp() })
+            }
+        }
+    }
+
+}
