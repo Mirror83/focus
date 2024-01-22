aa8f52901ae23e4396bc28b231e554ef1f389636
Mirror83 <glenochieng045@gmail.com>
2024-01-22 21:37:37 +0300

    Added the SettingsScreen composable

---
+++ composeApp/src/commonMain/kotlin/Settings.kt
@@ -0,0 +1,22 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SettingsScreen(onNavigateBack: () -> Unit) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text("Settings")
+        TextButton(onClick = { onNavigateBack() }) {
+            Text("Back")
+        }
+    }
+}
